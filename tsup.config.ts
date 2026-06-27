566b06e104954adff701a6685bd78958e9f89785
ae-michu <72276028+ae-michu@users.noreply.github.com>
2026-06-27 20:34:27 +0200

    update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,10 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
 	entry: ['src/index.ts'],
 	format: ['esm', 'cjs'],
-	dts: true,
-	clean: true,
 	external: ['react', 'react-dom'],
+	dts: true,
+	minify: true,
+	clean: true,
+	treeshake: true,
+	sourcemap: true
 });
